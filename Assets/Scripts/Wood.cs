8cce09ca51dfc1c60cdffa2fea6cd1595943e3da
wldxiaobai <944418001@qq.com>
2026-03-28 04:25:20 +0800

    first model

--- Assets/Scripts/Wood.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewWood", menuName = "Items/Wood")]
-public class Wood : ScriptableObject
-{
-    public string woodName = "Wood";
-    //public int durability = 1;
-    public Sprite icon;
-}
